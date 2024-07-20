0417127b7b2fdc9113d0a2b50a774dac023a753b
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-20 16:38:48 +0300

    ⚛️  feat: creating layout of dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,29 @@ import './App.css'
 import Home from './pages/home/Home'
 import Products from './pages/products/Products'
 import Users from './pages/users/Users'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
 
+  const router = createBrowserRouter(
+    [
+    {
+      path: '/',
+      element: <Home/>
+    },
+    {
+      path: 'users',
+      element: <Users/>
+    },
+    {
+      path: 'products',
+      element: <Products/>
+    }
+  ]
+  )
 
   return (
-    <>
-      <Home />
-      <Products />
-      <Users />
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
