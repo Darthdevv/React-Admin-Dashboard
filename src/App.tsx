4544e40c0efeffa900930ea151d4719f574ad653
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-20 18:11:43 +0300

    ⚛️  style: adding styes for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import './App.css'
+import './styles/global.scss'
 import Home from './pages/home/Home'
 import Products from './pages/products/Products'
 import Users from './pages/users/Users'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Error from './pages/error/Error'
 
 function App() {
 
-  const router = createBrowserRouter(
-    [
+  const router = createBrowserRouter([
     {
-      path: '/',
-      element: <Home/>
+      path: "/",
+      element: <MainLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+        },
+        {
+          path: "users",
+          element: <Users />,
+        },
+      ],
     },
-    {
-      path: 'users',
-      element: <Users/>
-    },
-    {
-      path: 'products',
-      element: <Products/>
-    }
-  ]
-  )
+  ]);
 
   return (
     <RouterProvider router={router}/>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,22 @@
-
+import Navbar from "../components/Navbar/Navbar"
+import Footer from "../components/Footer/Footer"
+import Menu from "../components/Menu/Menu"
+import {Outlet}  from 'react-router-dom'
 
 const MainLayout = () => {
   return (
-    <>
-      
-    </>
+    <div className="main">
+        <Navbar />
+      <div className="container">
+        <div className="menuContainer">
+          <Menu />
+        </div>
+        <div className="contentContainer">
+          <Outlet />
+        </div>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
