cb5e2ba25b7d5d344e7b9ddcdb2246c477b82b22
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-20 15:27:17 +0300

    ⚛️ first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Home from './pages/home/Home'
+import Products from './pages/products/Products'
+import Users from './pages/users/Users'
+
+function App() {
+
+
+  return (
+    <>
+      <Home />
+      <Products />
+      <Users />
+    </>
+  )
+}
+
+export default App
